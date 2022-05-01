7b3323d9c937bd92da393d54be51265487f128c8
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-01 17:08:43 +0600

    tailwind added and envorment setupfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home/Home';
+import DataNotFound from './component/DataNotFound/DataNotFound';
+import Footer from './component/Footer/Footer';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>This is app</h1>
+      <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='*' element={<DataNotFound></DataNotFound>}></Route>
+     </Routes>
+     <Footer></Footer>
     </div>
   );
 }
